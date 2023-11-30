f5d39e22db275e4155b2993459456c53b87962be
JakeOrona <jakeorona@gmail.com>
2023-11-30 15:24:22 -0600

    added 3 tests

--- TestAutomationPOC.py
+++ TestAutomationPOC.py
@@ -7,42 +7,68 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
-class WebAppTestCase1(unittest.TestSuite):
-    def setUp(self):
+class WebAppTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
         # set up url then driver
-        self.url = "https://linerider.com"
-        self.driver = webdriver.Chrome()
-        print("Chrome Driver Loaded -:" + self.url)
+        cls.url = "https://deepai.org/machine-learning-model/text2img"
+        cls.driver = webdriver.Chrome()
+        print("Chrome Driver Loaded -:" + cls.url)
 
-        # Wait for the search box to be visible
-        self.wait = WebDriverWait(self.driver, 100)
-
-    def test_start_game(self):
+    @classmethod
+    def tearDownClass(cls):
+        time.sleep(30)
+        cls.driver.quit()
+    
+    def setUp(self):
+        # Wait for the page to load
+        self.wait = WebDriverWait(self.driver, 7)
+    
+    def test_enter_prompt(self):
         try:
-            # click play
             self.driver.get(self.url)
+            print("Driver Loaded - " + self.url)
+            prompt_box = self.wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "model-input-text-input")))
+            assert prompt_box.is_displayed()
+            print("Found prompt box element")
+            # Clear the search box
+            prompt_box.clear()
+            print("Prompt Box Cleared")
 
-            # sleep 5 seconds for page load
-            time.sleep(5)
-            play_button = self.driver.find_element(By.XPATH,'//*[@id="content"]/div/div/div/div[2]/div[2]/button[2]/span[1]')
-            assert play_button.is_displayed()
-            print("Found play button element")
+            # Type "AI Image Generator" into the search box
+            prompt_box.send_keys("Toyota Truck Jump")
+            print("Added text 'Toyota Truck Jump' to search field")
 
-            # click play button
-            play_button.click()
-            print("Play button clicked")
-
-            # Delay the program for a certain amount of time
-            print("Delaying 100 seconds")
-            time.sleep(100)  # Wait for 100 seconds
+            expected_text = "Toyota Truck Jump"
+            prompt_text = prompt_box.text
+            # assert expected_text == prompt_text, f"Expected: {expected_text}, Actual: {prompt_text}"
 
         except AssertionError as e:
             print("AssertionError:", e)
 
-    #def test_draw_line(self):
-        #try:
-            # draw first line
+    def test_generate_prompt(self):
+        try:
+            # Submit the prompt
+            generate_button = self.wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="modelSubmitButton"]')))
+            assert generate_button.is_displayed()
+            print("\nFound Generate Button")
+            generate_button.click()
+            print("Generate button clicked")
 
+        except AssertionError as e:
+            print("AssertionError:", e)
 
+    def test_download_image(self):
+        try:
+            #download the generated image
+            download_button = self.wait.until(EC.visibility_of_element_located((By.XPATH,'//*[@id="download-model-image"]')))
+            assert download_button.is_displayed()
+            print("Found Download Button")
+            download_button.click()
+            print("Clicked Download Button")
 
-unittest.main(verbosity=2)
+        except AssertionError as e:
+            print("AssersionError:", e)
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
--- Test_Start_Game.py
+++ Test_Start_Game.py
@@ -1,6 +1,6 @@
 import time
 import unittest
-import Test Automation POC
+import TestAutomationPOC
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
@@ -8,7 +8,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
-class TestStartGame(unittest.TestCase)
+class TestStartGame(unittest.TestCase):
     def test_start_game(self):
             try:
                 # click play
